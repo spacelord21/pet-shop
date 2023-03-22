2cd869e1e59e4ef1abce7f5202714c0f9afc85dc
spacelord21 <incrediblespaceee@gmail.com>
2023-03-22 19:51:35 +0200

    persist products with expired time

--- src/entities/products/model/store.ts
+++ src/entities/products/model/store.ts
@@ -1,9 +1,21 @@
 import { createAlert, DEFAULT_ALERT_TIMEOUT } from "@entities/alert";
+import {
+  getDataFromLocaleStorage,
+  setDataInLocaleStorage,
+} from "@entities/persist";
 import { createEffect, createEvent, createStore } from "effector";
 import { getAllProducts } from "../api";
 import { TProduct } from "../types";
 
+const PERSIST_KEY = "PRODUCTS";
+// 10 min
+const DEFAULT_EXPIRE_TIME = 60 * 10;
+// 10 sec
+const DEFAULT_REFETCH_TIME = 10 * 1000;
+
 export const $products = createStore<TProduct[]>([]);
+
+export const fillProducts = createEvent<TProduct[]>();
 export const fetchProducts = createEvent();
 export const fetchProductsFx = createEffect<void, TProduct[], Error>(
   async () => {
@@ -11,9 +23,20 @@ export const fetchProductsFx = createEffect<void, TProduct[], Error>(
   }
 );
 
-fetchProducts.watch(() => fetchProductsFx());
+fetchProducts.watch(() => {
+  const data = getDataFromLocaleStorage<TProduct[]>(
+    PERSIST_KEY,
+    DEFAULT_EXPIRE_TIME
+  );
+  if (data !== undefined) {
+    fillProducts(data);
+  } else {
+    fetchProductsFx();
+  }
+});
 
 $products.on(fetchProductsFx.doneData, (_, payload) => payload);
+$products.on(fillProducts, (_, payload) => payload);
 
 fetchProductsFx.failData.watch((payload) => {
   createAlert({
@@ -23,11 +46,17 @@ fetchProductsFx.failData.watch((payload) => {
   });
 });
 
-let timeout: ReturnType<typeof setTimeout>;
+fetchProductsFx.doneData.watch((payload) => {
+  setDataInLocaleStorage<TProduct[]>(PERSIST_KEY, payload);
+});
+
+let refetchTimeout: ReturnType<typeof setTimeout>;
 
 fetchProductsFx.fail.watch(() => {
-  clearTimeout(timeout);
-  timeout = setTimeout(() => {
-    fetchProductsFx();
-  }, 10000);
+  clearTimeout(refetchTimeout);
+  if ($products.getState().length === 0) {
+    refetchTimeout = setTimeout(() => {
+      fetchProductsFx();
+    }, DEFAULT_REFETCH_TIME);
+  }
 });
