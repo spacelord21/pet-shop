bb1e0628a994468a3319b65b1c755c7a3523fc0f
spacelord21 <incrediblespaceee@gmail.com>
2023-03-27 14:05:26 +0200

    remove unused imports

--- src/pages/product/ui/molecules/description-slice/description-slice.tsx
+++ src/pages/product/ui/molecules/description-slice/description-slice.tsx
@@ -78,7 +78,12 @@ export const DescriptionSlice = ({
         </Button>
       </Container>
       <Separator width={isNotDesktop ? 100 : 65} />
-      <Text variant="body14" isActive={isPressed} ref={itemRef}>
+      <Text
+        variant="body14"
+        isActive={isPressed}
+        ref={itemRef}
+        className="desc-content"
+      >
         {content}
       </Text>
     </>
